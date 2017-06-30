ceacb4c2eaabc4de0a0a94c0bbc87ef07185afe2
TOCHI\Burak <burakdogmus@gmail.com>
2017-06-30 15:45:50 +0300

    Tank AimingComponentClass

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
-#include "Tank.generated.h"
+#include "Tank.generated.h" //Put new includes obove
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -13,6 +13,9 @@ class BATTLETANK_API ATank : public APawn
 
 public:
 	void AimAt(FVector HitLocation);
+
+protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
 	// Sets default values for this pawn's properties
